8be04a0bb8513eafe8d9c46ab98329f494bf2481
William Martin <will.st4@gmail.com>
2011-06-04 16:11:44 +0100

    created sources

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Will
+ *
+ * Created on 04 June 2011, 16:05
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello";
+
+    return 0;
+}
+
