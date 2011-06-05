bf57c847bcd2d7a456f261314a070acfcd292785
William Martin <will.st4@gmail.com>
2011-06-05 12:44:52 +0100

    created input interface

---
+++ src/net/Input.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Input.h
+ * Author: Will
+ *
+ * Created on 05 June 2011, 12:43
+ */
+
+#ifndef INPUT_H
+#define	INPUT_H
+
+/**
+ * Input object for neurons.
+ * 
+ */
+class Input {
+public:
+    Input();
+    virtual ~Input();
+private:
+};
+
+#endif	/* INPUT_H */
+
