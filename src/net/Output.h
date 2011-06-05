2c37eafdf81a5dc8f4515cff93b54383f3f28173
William Martin <will.st4@gmail.com>
2011-06-05 12:39:24 +0100

    created output interface

---
+++ src/net/Output.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Output.h
+ * Author: Will
+ *
+ * Created on 05 June 2011, 12:28
+ */
+
+#ifndef OUTPUT_H
+#define	OUTPUT_H
+
+/**
+ * Output object from neurons.
+ * 
+ */
+class Output {
+public:
+    Output();
+    virtual ~Output();
+private:
+};
+
+#endif	/* OUTPUT_H */
+
