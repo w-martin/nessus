1026aa46b7fbf8b22a55a12d38d40ca52a5443b6
William Martin <will.st4@gmail.com>
2011-06-08 17:24:20 +0100

    created neuron factory class

---
+++ src/nets/NeuronFactory.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   NeuronFactory.cpp
+ * Author: Will
+ * 
+ * Created on 08 June 2011, 16:23
+ */
+
+#include "NeuronFactory.h"
+
+NeuronFactory::NeuronFactory() {
+}
+
+NeuronFactory::~NeuronFactory() {
+}
+
---
+++ src/nets/NeuronFactory.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   NeuronFactory.h
+ * Author: Will
+ *
+ * Created on 08 June 2011, 16:23
+ */
+
+#ifndef NEURONFACTORY_H
+#define	NEURONFACTORY_H
+
+class NeuronFactory {
+public:
+    NeuronFactory();
+    virtual ~NeuronFactory();
+private:
+
+};
+
+#endif	/* NEURONFACTORY_H */
+
