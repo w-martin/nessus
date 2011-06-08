9610c8b4700e67983923e4278e7deb14f2888124
William Martin <will.st4@gmail.com>
2011-06-08 17:22:26 +0100

    added trainer source file

---
+++ src/trainer/Trainer.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   Trainer.h
+ * Author: Will
+ *
+ * Created on 08 June 2011, 15:53
+ */
+
+#include "Trainer.h"
+#include <time.h>
+#include <stdlib.h>
+
+Trainer::Trainer() {
+}
+
+Trainer::~Trainer() {
+}
+
+void Trainer::initWeights(Neuron* n) {
+    setRandomWeights(n);
+}
+
+void Trainer::setRandomWeights(Neuron* n) {
+    Weights *w = n->getWeights();
+    srand(time(NULL));
+    for (int i = 0; i < w->getSize(); i++) {
+        int random = rand();
+        float multiplier = random / ((float) (2 * RAND_MAX));
+        w->setMultiplier(i, multiplier);
+    }
+}
