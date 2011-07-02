f5b1d4d0aee351639e32b9876adf571968553bfa
William Martin <will.st4@gmail.com>
2011-07-02 01:04:37 +0100

    added tests

---
+++ test/model/exceptions/EmptyVectorExceptionTest.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   EmptyVectorExceptionTest.cpp
+ * Author: will
+ * 
+ * Created on July 2, 2011, 12:55 AM
+ */
+
+#include "gtest/gtest.h"
+#include "model/exceptions/EmptyVectorException.h"
+
+#define message "test message"
+
+namespace {
+
+    class EmptyVectorExceptionTest : public ::testing::Test {
+    protected:
+
+        EmptyVectorExceptionTest() {
+            testException = new EmptyVectorException(message);
+        }
+
+        virtual ~EmptyVectorExceptionTest() {
+            delete testException;
+        }
+        EmptyVectorException *testException;
+    };
+
+    /*
+     * Tests whether the <code>Exception</code>'s message was set correctly.
+     * 
+     */
+    TEST_F(EmptyVectorExceptionTest, MessageTest) {
+        EXPECT_STREQ(message, testException->what());
+    }
+
+    /*
+     * Tests whether the default message is set correctly.
+     * 
+     */
+    TEST_F(EmptyVectorExceptionTest, DefaultMessageTest) {
+        delete testException;
+        testException = new EmptyVectorException();
+
+        EXPECT_STREQ(EMPTY_VECTOR_MESSAGE, testException->what());
+    }
+}
---
+++ test/model/exceptions/IncorrectInputExceptionTest.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   IncorrectInputExceptionTest.cpp
+ * Author: will
+ * 
+ * Created on July 2, 2011, 1:01 AM
+ */
+
+#include "gtest/gtest.h"
+#include "model/exceptions/IncorrectInputException.h"
+
+#define message "test message"
+
+namespace {
+
+    class IncorrectInputExceptionTest : public ::testing::Test {
+    protected:
+
+        IncorrectInputExceptionTest() {
+            testException = new IncorrectInputException(message);
+        }
+
+        virtual ~IncorrectInputExceptionTest() {
+            delete testException;
+        }
+        IncorrectInputException *testException;
+    };
+
+    /*
+     * Tests whether the <code>Exception</code>'s message was set correctly.
+     * 
+     */
+    TEST_F(IncorrectInputExceptionTest, MessageTest) {
+        EXPECT_STREQ(message, testException->what());
+    }
+
+    /*
+     * Tests whether the default message is set correctly.
+     * 
+     */
+    TEST_F(IncorrectInputExceptionTest, DefaultMessageTest) {
+        delete testException;
+        testException = new IncorrectInputException();
+
+        EXPECT_STREQ(INCORRECT_INPUT_MESSAGE, testException->what());
+    }
+}
