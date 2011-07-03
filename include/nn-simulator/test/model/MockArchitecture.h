76bbcc716dafd211d62966b51def657c3eb0d244
William Martin <will.st4@gmail.com>
2011-07-03 13:10:15 +0100

    added some mock classes

---
+++ include/nn-simulator/test/model/MockArchitecture.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   MockArchitecture.h
+ * Author: Will
+ * 
+ * Created on 03 July, 09:56
+ */
+
+#include "gmock/gmock.h"
+#include "nn-simulator/main/model/Architecture.h"
+#include "nn-simulator/test/functions/MockOutputFunction.h"
+
+class MockArchitecture : public Architecture {
+public:
+
+    MockArchitecture() : Architecture(0,
+    auto_ptr<OutputFunction>(new MockOutputFunction()),
+    auto_ptr<Trainer>(new Trainer())) {
+    }
+};
---
+++ include/nn-simulator/test/model/MockLayer.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   MockLayer.h
+ * Author: Will
+ * 
+ * Created on 03 July, 10:06
+ */
+
+#include "gmock/gmock.h"
+#include "nn-simulator/main/model/Layer.h"
+
+class MockLayer : public Layer {
+public:
+
+    MockLayer() : Layer(0, false) {
+    }
+    MOCK_METHOD1(processInput,
+            Input*(Input *input));
+};
