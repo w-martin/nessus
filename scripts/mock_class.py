00e6473acfc3c12b486985da3ad280505878cd75
William Martin <will.st4@gmail.com>
2011-07-06 00:04:09 +0100

    completed python script

--- scripts/mock_class.py
+++ scripts/mock_class.py
@@ -17,6 +17,7 @@
 
 __author__ = 'will.st4@gmail.com (William Martin)'
 
+import os
 import sys
 
 ## Definitions
@@ -26,10 +27,28 @@ mainDir = headerDir + "main/"
 testDir = headerDir + "test/"
 # gmock script
 gmockScript = "gmock-1.6.0/scripts/generator/gmock_gen.py"
+# function
+def usage():
+    print("Usage:\n    scripts/mock_class.py header.h\nThe generated Mock class will be placed in the include/nn-simulator/test directory.")
+def fileAbsent(fileName):
+    print("Error. File '" + fileName + "' does not exist.")
 
 if len(sys.argv) > 1:
     source = sys.argv[1]
-    
-
+    if not (mainDir in source):
+        source = mainDir + source
+    if os.path.isfile(source):
+        try:
+            open(source)
+            dest = source.replace(mainDir, testDir)
+            className = source[source.rindex('/')+1:source.rindex('.')]
+            dest = dest[:source.rindex('/')+1] + "Mock" + className + ".h"
+            command = gmockScript + " " + source + " " + className + " >> " + dest
+            #print command
+            os.popen(command)
+        except IOError:
+            fileAbsent(source)
+    else:
+        fileAbsent(source)
 else:
-    print("Usage:\n    scripts/mock_class.py header.h\nWhere the header location is relative to include/nn-simulator/main/\n")
+    usage()
